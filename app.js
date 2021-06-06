1cb01bbfc188e375570eeb76edfffacb3e2be4b3
Shivu02062002 <75877929+Shivu02062002@users.noreply.github.com>
2021-06-06 09:05:47 +0530

    added the dependency of mongoose server and made some updates to the code

--- app.js
+++ app.js
@@ -2,13 +2,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
-    console.log("Server successfully started at port 3000.");
-})
+//After we require the mongoose modules, it is time to connect it to the MongoDB database server
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+//Rendering the home page
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+//Rendering the register page
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+//Rendering the login page
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+//Initialising the port where the website is hosted
+app.listen(3000, function () {
+    console.log("Server successfully started at port 3000.")
+});
